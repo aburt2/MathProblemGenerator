cd3b4e60fe06c46b841569ff0c228b067d2f96ff
Albert-Ngabo Niyonsenga <u6352280@anu.edu.au>
2018-04-15 19:08:51 +1000

    Changed Linear algebra problems

--- linear_algebra_problems.py
+++ linear_algebra_problems.py
@@ -59,13 +59,20 @@ def random_math():
     solution = np.linalg.solve(A,B)
     return a1,b1,c1,a2,b2,c2,solution
     
-def generate_math_problems(problems):
+def generate_math_problems():
     '''
     Generates a number of linear algebra problems. 
     The user must answer at 70% of those problems correctly
     '''
     correct_answers = 0
     attempted_problems = 0
+    while True:
+        problems = input('Number of problems you want to attempt:')
+        try:
+            problems = int(problems)
+            break
+        except ValueError:
+            print('Please enter an integer')
     while attempted_problems < problems:
         a1,b1,c1,a2,b2,c2,solution = random_math() 
         show_problem(a1,b1,c1,a2,b2,c2)
@@ -91,6 +98,6 @@ def generate_math_problems(problems):
         else:
             print('Please responde with Yes or No')
 
-generate_math_problems(10)     
+generate_math_problems()     
 
     
